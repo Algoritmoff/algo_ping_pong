cfbc475ba63bfcb103c13480e6b4c7c1a8aa4999
Algoritmoff <131158803+Algoritmoff@users.noreply.github.com>
2023-12-19 19:27:01 +0500

    Update ping_pong.py
    v 2.0

--- ping_pong.py
+++ ping_pong.py
@@ -29,7 +29,7 @@ class Player(GameSprite):
     # метод для управления спрайтом стрелками клавиатуры
     def update_r(self):
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.y > 5:
+        if keys[K_UP] and self.rect.y > 4:
             self.rect.y -= self.speed
         if keys[K_DOWN] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
